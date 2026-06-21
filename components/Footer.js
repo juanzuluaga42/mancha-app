52aed283d98e05cbc29841b6b62f2e47ef565ef5
juanzuluaga42 <khcb6dm64k@privaterelay.appleid.com>
2026-06-20 21:12:14 -0500

    Update Footer.js

--- components/Footer.js
+++ components/Footer.js
@@ -8,6 +8,8 @@ export default function Footer() {
       <Splat width="110px" height="95px" top="-30px" left="-35px" color="lilac" rotate={16} radius="r2" />
       <Splat width="60px" height="55px" bottom="18%" right="-25px" color="red" rotate={-8} radius="r3" />
       <Splat width="55px" height="50px" top="10%" right="30%" color="red" rotate={12} radius="r4" />
+      <Splat width="50px" height="46px" top="35%" left="40%" color="yellow" rotate={-10} radius="r2" />
+      <Splat width="45px" height="42px" bottom="8%" left="20%" color="lilac" rotate={14} radius="r3" />
       <div className="wrap">
         <div className="footer-top">
           <div className="footer-brand">
@@ -19,6 +21,8 @@ export default function Footer() {
               <h5>Explorar</h5>
               <Link href="/#artistas">Temporada actual</Link>
               <Link href="/#favoritos">Favoritos</Link>
+              <Link href="/#notas">Notas</Link>
+              <Link href="/#tips">Tips</Link>
               <Link href="/postular">¿Eres artista?</Link>
             </div>
             <div className="footer-col">
